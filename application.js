3ec94ebfd68451f009f351863bc769c29105e9f7
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-09-30 14:17:52 -0500

    Implement mvp

--- application.js
+++ application.js
@@ -2,13 +2,47 @@ $( document ).ready(function() {
     MarkdownWidget("#textarea1", "#textarea2");
 });
 
-function MarkdownWidget(arg1, arg2){
+function MarkdownWidget (arg1, arg2){
+
   $(arg1).on('keyup', function(event){
-    $(arg2).val($(arg1).val());
+
+    var result = markdown_mvp($(this));
+
+    $(arg2).html(result);
+
   });
 };
 
-//  I don't know how to implement the italic/bold
-// Maybe a conditional of some sort?, My textbox
-// doesn't do styling... I'm not sure how to solve
-// this. Will come back to it later.
+function markdown_mvp (arg) {
+
+  var result = arg.val().replace(/\*\*(\w+)\*\*/, "<strong> $1 </strong>");
+
+  var result = result.replace(/\_(\w+)\_/, "<u> $1 </u>");
+
+  var result = result.replace(/\*(\w+)\*/, "<i> $1 </i>");
+
+  return result;
+}
+
+// Markdown implementation
+// function MarkdownWidget (arg1, arg2){
+//   $(arg1).on('keyup', function(){
+//
+//     var display = $(this).val();
+//
+//     html_content = markdown.toHTML( display, "Maruku" );
+//
+//     $(arg2).val(html_content);
+//   })
+// };
+
+
+// Drafts
+
+// This works, but dirty.
+// var $c = $(this).val().replace(/\*\*(\w+)\*\*/, "<strong> $1 </strong>").replace(/\_(\w+)\_/, "<u> $1 </u>").replace(/\*(\w+)\*/, "<i> $1 </i>")
+
+// This is less bad
+// var $c = $(this).val().replace(/\*\*(\w+)\*\*/, "<strong> $1 </strong>");
+// var $c = $c.replace(/\_(\w+)\_/, "<u> $1 </u>");
+// var $c = $c.replace(/\*(\w+)\*/, "<i> $1 </i>");
