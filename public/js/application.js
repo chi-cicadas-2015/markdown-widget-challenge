7aeb5a1294a29b92e243d1d4265237db051516c3
Madelyn Kasula <madelynkasula@gmail.com>
2015-09-30 14:50:31 -0500

    Some italics functionality - need to add multiple case functionality

--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $('#input').on('input', function() {
     var $text = $(this).val();
     var boldWord = $text.match(/__.+__/);
-    console.log(boldWord);
+    var italicWord = $text.match(/_.+_/);
     $text = $text.replace(boldWord, '<strong>$&</strong>');
+    $text = $text.replace(italicWord, '<em>$&</em>');
 
     $('#markdownified').html($text);
   });
