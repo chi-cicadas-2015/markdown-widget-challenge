429dd7ebeb7a50bbcaaa6c075be592d86ea8ba92
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-09-30 14:22:58 -0500

    Finish challenge with mvp implementation and also library for markdown, pretty awesome

--- application.js
+++ application.js
@@ -2,39 +2,50 @@ $( document ).ready(function() {
     MarkdownWidget("#textarea1", "#textarea2");
 });
 
-function MarkdownWidget (arg1, arg2){
+// // This is my implementation, works, but starts to fail when you want to
+// // nest markups (eg bold and also italic)
 
-  $(arg1).on('keyup', function(event){
-
-    var result = markdown_mvp($(this));
-
-    $(arg2).html(result);
-
-  });
-};
-
-function markdown_mvp (arg) {
-
-  var result = arg.val().replace(/\*\*(\w+)\*\*/, "<strong> $1 </strong>");
-
-  var result = result.replace(/\_(\w+)\_/, "<u> $1 </u>");
-
-  var result = result.replace(/\*(\w+)\*/, "<i> $1 </i>");
-
-  return result;
-}
+// function MarkdownWidget (arg1, arg2){
+//
+//   $(arg1).on('keyup', function(event){
+//
+//     var result = markdown_mvp($(this));
+//
+//     $(arg2).html(result);
+//
+//   });
+// };
+//
+// function markdown_mvp (arg) {
+//
+//   var result = arg.val().replace(/\*\*(\w+)\*\*/, "<strong> $1 </strong>");
+//
+//   var result = result.replace(/\_(\w+)\_/, "<u> $1 </u>");
+//
+//   var result = result.replace(/\*(\w+)\*/, "<i> $1 </i>");
+//
+//   return result;
+// }
 
 // Markdown implementation
-// function MarkdownWidget (arg1, arg2){
-//   $(arg1).on('keyup', function(){
-//
-//     var display = $(this).val();
-//
-//     html_content = markdown.toHTML( display, "Maruku" );
-//
-//     $(arg2).val(html_content);
-//   })
-// };
+function MarkdownWidget (arg1, arg2){
+  
+  $(arg1).on('keyup', function(){
+
+    var display = $(this).val();
+
+    html_content = markdown.toHTML( display, "Maruku" );
+
+    $(arg2).html(html_content);
+
+  })
+
+};
+
+
+
+
+
 
 
 // Drafts
