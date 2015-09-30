27f132c54f507be46b44b2925bce76270b6b9890
chris zajac & Rodica Trifan <pair+cMzeta.trodicaro@devbootcamp.com>
2015-09-30 14:18:48 -0500

    Render text in the preview area

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+$(document).ready(function() {
+
+$("#source-id").keyup(function() {
+  var textValue = $(this).val();
+  // MarkdownWidget function here:
+  $("#preview-div p").text(textValue);
+
+
+// function MarkdownWidget(source, preview) {
+
+
+});
+
+// };
+
+
+
+});
---
+++ js/markdownWidget.js
@@ -0,0 +1,5 @@
+MarkdownWidget.prototype.markdownText = function(textValue){
+  forEach
+}
+
+
